664b9c323ae4875c752138bc9ee2bc8cb5f0cc78
Егор Иватько <ivatkoegor@gmail.com>
2018-01-19 16:01:54 +0000

    Update Interpreter.cs

--- Recount.Core/Interpreter.cs
+++ Recount.Core/Interpreter.cs
@@ -28,7 +28,7 @@ namespace Recount.Core
 
             state = state.MoveToFinalState(expression.Length, _stack);
             state.Execute(_stack);
-            return _stack.GetResult()2;
+            return _stack.GetResult();
         }
     }
 }

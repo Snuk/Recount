852150d531db210968501c5871619fc2beed7a03
Иватько Егор <e.ivatko@devinotele.com>
2018-01-25 23:24:37 +0300

    #13 Provider for saving funcs and vars

---
+++ Recount.DataAccess/Providers/MongoFunctionsProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+using Recount.Core.Functions;
+using Recount.Core.Identifiers;
+using Recount.Core.Lexemes;
+
+namespace Recount.DataAccess.Providers
+{
+    public class MongoFunctionsProvider : IFunctionsProvider
+    {
+        //mongodb-1-servers-vm-0:27017
+        private readonly IMongoCollection<Function> _functionsCollection;
+
+        public MongoFunctionsProvider()
+        {
+            _functionsCollection = new MongoClient(new MongoUrl("mongodb://mongodb-1-servers-vm-0:27017")).GetDatabase("recount")
+                .GetCollection<Function>("functions");
+        }
+
+        public void Add(Function function)
+        {
+            _functionsCollection.ReplaceOne(f => f.Name.Body == function.Name.Body, function, new UpdateOptions { IsUpsert = true });
+        }
+
+        public Function Get(Variable name)
+        {
+            return _functionsCollection.Find(f => f.Name.Body == name.Body).Project<Function>(Builders<Function>.Projection.Exclude("_id"))
+                .First();
+        }
+
+        public List<Function> GetAll()
+        {
+            return _functionsCollection.Find(FilterDefinition<Function>.Empty)
+                .Project<Function>(Builders<Function>.Projection.Exclude("_id")).ToList();
+        }
+
+        public void Delete(Variable name)
+        {
+            _functionsCollection.DeleteOneAsync(f => f.Name.Body == name.Body);
+        }
+    }
+}
---
+++ Recount.DataAccess/Providers/MongoVariablesProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using Recount.Core.Identifiers;
+using Recount.Core.Lexemes;
+using Recount.Core.Numbers;
+
+namespace Recount.DataAccess.Providers
+{
+    public class MongoVariablesProvider : IVariablesProvider
+    {
+        //mongodb-1-servers-vm-0:27017
+        private readonly IMongoCollection<VariableEntity> _variablesCollection;
+
+        public MongoVariablesProvider()
+        {
+            _variablesCollection = new MongoClient(new MongoUrl("mongodb://mongodb-1-servers-vm-0:27017")).GetDatabase("recount")
+                .GetCollection<VariableEntity>("variables");
+        }
+
+        public void Add(Variable name, Number value)
+        {
+            _variablesCollection.ReplaceOne(
+                v => v.Name.Body == name.Body,
+                new VariableEntity { Name = name, Value = value },
+                new UpdateOptions { IsUpsert = true });
+        }
+
+        public Number Get(Variable name)
+        {
+            return _variablesCollection.Find(v => v.Name.Body == name.Body)
+                .Project<VariableEntity>(Builders<VariableEntity>.Projection.Exclude("_id")).First().Value;
+        }
+
+        public Dictionary<Variable, Number> GetAll()
+        {
+            return _variablesCollection.Find(FilterDefinition<VariableEntity>.Empty)
+                .Project<VariableEntity>(Builders<VariableEntity>.Projection.Exclude("_id")).ToEnumerable()
+                .ToDictionary(v => v.Name, v => v.Value);
+        }
+
+        public void Delete(Variable name)
+        {
+            _variablesCollection.DeleteOne(v => v.Name.Body == name.Body);
+        }
+
+        private class VariableEntity
+        {
+            public Variable Name { get; set; }
+
+            public Number Value { get; set; }
+        }
+    }
+}
